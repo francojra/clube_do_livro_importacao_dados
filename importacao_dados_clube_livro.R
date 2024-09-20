a64f0dfee79753e0443f0b79bd590455e112bb6f
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-09-19 22:20:02 -0300

    Add files via upload

--- importacao_dados_clube_livro.R
+++ importacao_dados_clube_livro.R
@@ -15,3 +15,17 @@ library(tidyverse)
 ## e valores separados por ponto.
 ## No Brasil, usamos o csv2, onde as colunas são separadas por ponto
 ## e vírgula e os valores separados por vírgula.
+
+## Funções do pacote readr
+
+read_csv()
+read_csv2()
+
+## IMPORTANTE: a primeira etapa é verificar onde está o seu diretório
+## de trabalho
+
+getwd() # Para verificar o diretório
+setwd() # Para especificar seu diretório, onde estão seus arquivos
+
+cardio <- read.csv2("cardio_train.csv")
+view(cardio)
